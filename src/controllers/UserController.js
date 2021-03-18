9b78c000caa1415ac92229d20e286b9ab0d00f24
João <joaomsfreis@gmail.com>
2021-03-18 18:23:07 -0300

    Login do Usuário.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,7 @@
 const pg = require('../postgres').pool;
 const bcrypt = require('bcrypt');
 const {v4: uuidv4} = require('uuid');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     register: async (req, res) => {
@@ -18,7 +19,7 @@ module.exports = {
                 if (err) return res.status(500).send({err});
 
                 if (result.rows.length > 0) {
-                    res.status(409).send({
+                    return res.status(409).send({
                         message: "Email já cadastrado!"
                     });
                 } else {
@@ -41,11 +42,64 @@ module.exports = {
                                 }
                             }
 
-                            res.status(201).send(response);
-
+                            return res.status(201).send(response);
+                            client.end();
                         })
                     });
                 }
+
+                client.end();
+            })
+        });
+    },
+
+    login: async (req, res) => {
+        await pg.connect((err, client) => {
+
+            if (err) return res.status(500).send({err});
+
+            const query = {
+                text: 'SELECT * FROM users WHERE email = $1',
+                values: [req.body.email],
+            }
+
+
+            client.query(query, (err, result) => {
+                if (err) return res.status(500).send({err});
+
+                if (result.rows.length < 1) {
+                    return res.status(401).send({
+                        message: "Falha na autenticação!"
+                    });
+                } else {
+                    bcrypt.compare(req.body.password, result.rows[0].password, (err, resultBcrypt) => {
+                        if (err) return res.status(401).send({
+                            message: "Falha na autenticação!"
+                        });
+
+
+                        if (resultBcrypt) {
+                            const token = jwt.sign({
+                                    user_id: result.rows[0].id,
+                                    email: result.rows[0].email,
+                                },
+                                process.env.JWT_KEY,
+                                {
+                                    expiresIn: '1h'
+                                }
+                            );
+                            return res.status(200).send({
+                                message: "Autenticado com sucesso!",
+                                token
+                            });
+                        }
+
+                        return res.status(401).send({
+                            message: "Falha na autenticação!"
+                        });
+                    });
+                }
+                client.end();
             })
         });
     },
